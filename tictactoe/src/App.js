ced54f4c0f88b633573a69692f89bec2d2f96a38
brian becker <brianbecker@brians-Air.domain>
2019-04-18 10:22:49 -0600

    first commit

---
+++ tictactoe/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./board.css";
+
+class board extends Component {
+  render() {
+    return <header />;
+  }
+}
+
+export default board;
---
+++ tictactoe/src/board.js
@@ -0,0 +1,23 @@
+
+import React, { Component } from "react";
+import ReactDOM from"react-dom";
+  
+class board extends Component.react {
+Component(prop)
+super(prop) {
+
+  
+
+<div className="container">
+      <div className="box1" ></div>
+      <div className="box2" ></div>
+      <div className="box3" ></div>
+      <div className="box4" ></div>
+      <div className="box5" ></div>
+      <div className="box6" ></div>
+      <div className="box7" ></div>
+      <div className="box8" ></div>
+      <div className="box9" ></div>
+    </div>
+};
+}
