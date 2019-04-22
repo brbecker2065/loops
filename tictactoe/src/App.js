063afbce098fe67e33085147927258f50ed41e7c
brian becker <brianbecker@brians-Air.domain>
2019-04-21 21:23:03 -0600

    first

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,35 +1,38 @@
-import React, {
-  Component
-} from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from "react";
+
 import "./App.css";
 class App extends Component {
-
   render() {
-    return
-
-    <div className = "container" >
-      <
-      div className = "box1" / >
-      <
-      div className = "box2" / >
-      <
-      div className = "box3" / >
-      <
-      div className = "box4" / >
-      <
-      div className = "box5" / >
-      <
-      div className = "box6" / >
-      <
-      div className = "box7" / >
-      <
-      div className = "box8" / >
-      <
-      div className = "box9" / >
-      <
-      /div>;
+    return (
+      <div className="container">
+        <div className="box1" />
+        <div className="box2" />
+        <div className="box3" />
+        <div className="box4" />
+        <div className="box5" />
+        <div className="box6" />
+        <div className="box7" />
+        <div className="box8" />
+        <div className="box9" />
+        <div className="xtoken" />
+        <button value="x">
+          x
+          <img
+            src="http://i.imgur.com/60PVLis.png"
+            width="25"
+            height="25"
+            alt=""
+          />
+          <span className="xtoken" />
+        </button>
+        <button value="o">
+          o<span className="otoken" />
+        </button>
+      </div>
+    );
   }
 }
+
+addEventHandler.button("click"=button(value)) 
 
 export default App;
